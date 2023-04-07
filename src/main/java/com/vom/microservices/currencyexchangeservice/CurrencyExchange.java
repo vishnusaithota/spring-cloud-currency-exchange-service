959f0a7254b6b3b220bb38396418ddd86ec7e636
tnvrravish <vt7@njit.edu>
2023-04-07 12:40:09 -0400

    added basic controller

---
+++ src/main/java/com/vom/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,61 @@
+package com.vom.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private  String environment;
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public CurrencyExchange(long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public CurrencyExchange() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+}
