a3365308e9ce73aacdb6b321c59fad61da2b53ae
tnvrravish <vt7@njit.edu>
2023-04-07 13:55:25 -0400

    configured Environment varable to get port number automatically

--- src/main/java/com/vom/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/vom/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.vom.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,18 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+//    used to get environment using spring
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retreiveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to){
 
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 
 }
